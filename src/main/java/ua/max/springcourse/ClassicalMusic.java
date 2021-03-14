527bb4f47331fa65b20fdd719a617825549045eb
KovalchukMax <kovalchukmax@gmail.com>
2021-03-14 17:11:32 +0200

    Spring Framework - Annotation - @Autowired - Setter

--- src/main/java/ua/max/springcourse/ClassicalMusic.java
+++ src/main/java/ua/max/springcourse/ClassicalMusic.java
@@ -2,7 +2,6 @@ package ua.max.springcourse;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
