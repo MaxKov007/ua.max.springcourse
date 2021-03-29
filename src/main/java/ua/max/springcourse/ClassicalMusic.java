8e24e527e3473f811f5cc87ca345d058e8bc225f
KovalchukMax <kovalchukmax@gmail.com>
2021-03-29 13:51:29 +0300

    Spring Framework - Annotation - @PostConstruct, @PreDestroy

--- src/main/java/ua/max/springcourse/ClassicalMusic.java
+++ src/main/java/ua/max/springcourse/ClassicalMusic.java
@@ -1,14 +1,25 @@
 package ua.max.springcourse;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public List<String> getSong() {
         List<String> classicalList = new ArrayList<>();
--- src/main/java/ua/max/springcourse/TestSpring.java
+++ src/main/java/ua/max/springcourse/TestSpring.java
@@ -17,11 +17,6 @@ public class TestSpring {
 
         musicPlayer.playMusic(MusicGenre.ROCK);
 
-        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
-
-        System.out.println(classicalMusic1 == classicalMusic2);
-
         context.close();
     }
 }
